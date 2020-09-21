ff4a511b57215487412e29997ccbfbb8048bb9ef
Ishaan Masil <darkknightishaan@gmail.com>
2020-09-21 14:39:24 +0530

    Uploaded Project

---
+++ GrapplyGun/Assets/Scripts/Grapple.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    public Camera fpsCam;
+    public float range = 300f;
+
+    public GameObject grappler;
+
+    public Rigidbody grapplerRb;
+
+    public SpringJoint springJoint;
+
+    private void Start()
+    {
+        grapplerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        springJoint.anchor = grappler.transform.position;
+
+        if (Input.GetButtonDown("Fire1"))
+            Grappler();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Grappler()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(grappler.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.DrawLine(grappler.transform.position, hit.point, Color.cyan, 1000f);
+
+            springJoint.maxDistance = Vector3.Distance(grappler.transform.position, hit.point);
+            springJoint.minDistance = 2;
+
+            springJoint.connectedAnchor = hit.point;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ GrapplyGun/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    private WeaponSwitching weaponSwitcher;
+
+    public int selectedWeapon;
+
+    public float speed;
+    public float gravity = -11f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float jumpHeight = 3.0f;
+
+    bool isGrounded;
+
+    private void Start()
+    {
+        weaponSwitcher = GetComponentInChildren<WeaponSwitching>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectedWeapon = weaponSwitcher.selectedWeapon;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (selectedWeapon == 1)
+        {
+            speed = 5;
+            gravity = -20;
+        }
+        else if (selectedWeapon == 0)
+        {
+            speed = 10;
+            gravity = -11f;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
